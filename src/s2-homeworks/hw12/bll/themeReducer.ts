b3f295917a2aa7e7c3fcaa8621d3d3ad0a2af9cc
MariaStepanyan <drmariastepanyan@gmail.com>
2024-04-15 13:38:41 +0300

    hw 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,32 @@
+export type initStatetype = {
+  themeId: number
+}
+
 const initState = {
-    themeId: 1,
+  themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+  state = initState,
+  action: ChangeThemeIdType
+): initStatetype => {
+  // fix any
+  switch (action.type) {
+    case 'SET_THEME_ID':
+      return { ...state, themeId: action.id }
+    // дописать
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ChangeThemeIdType => ({
+  type: 'SET_THEME_ID',
+  id,
+}) // fix any
+
+type ChangeThemeIdType = {
+  type: 'SET_THEME_ID'
+  id: number
+}
